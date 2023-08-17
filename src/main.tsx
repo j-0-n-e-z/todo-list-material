ff36dcd894af335266514ffbfd3d98bed46ff33c
j-0-n-e-z <j000neeez@gmail.com>
2023-08-17 12:40:28 +0500

    hot-fix: import store from shorthand path

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
+import { store } from '@/redux'
+
 import App from './App.tsx'
-import { store } from './app/store.ts'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
