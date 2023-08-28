8a2cba62e85ec5af5f75c810d012c2cb35202e38
j-0-n-e-z <j000neeez@gmail.com>
2023-08-28 16:24:24 +0500

    upd eslint and gitignore

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
 		{
 			files: ['*.ts', '*.tsx'],
 			parserOptions: {
-				project: ['./tsconfig.json', './tsconfig.node.json']
+				project: ['client/tsconfig.json', 'client/tsconfig.node.json']
 			}
 		}
 	],
